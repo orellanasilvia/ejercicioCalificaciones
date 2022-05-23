7d18b30d3516945ff10b67c6c3e6cf705550ad3d
orellanasilvia <104109498+orellanasilvia@users.noreply.github.com>
2022-05-23 15:34:46 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let prom, n1,n2,n3, sumaNota :number ;
+let nombreAlumno = prompt("Ingrese nombre del alumno:");
+while (nombreAlumno!== "") {
+  n1 =Number (prompt("ingrese calificacion de Prectica"));
+  console.log("Calificacion Practica :" + n1);
+  n2 =Number (prompt("ingrese calificacion de Problemas"));
+  console.log("Calificacion Problema :" + n2);
+  n3 =Number (prompt("ingrese calificacion de Teorica"));
+  console.log("Calificacion Teorica :" + n3);
+  if (n1> -1 && n2 > -1 && n3 > -1 && n1<==10 && n2<==10 && n3<==10) {
+    prom= n1 *0.10 + n2*0.50+ n3* 0.40;
+    console.log ("Nota final es :" + prom);
+    else {
+      console.log (" error , ud. ingreso un valor invalido");
+    }
+  }
+}
