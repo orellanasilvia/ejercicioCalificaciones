5f3120d2d2529b72a468cb8d43969849cd7ac7bd
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-05-25 12:15:54 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-let prom, n1,n2,n3, sumaNota :number ;
+let prom, n1, n2, n3: number;
 let nombreAlumno = prompt("Ingrese nombre del alumno:");
-while (nombreAlumno!== "") {
-  n1 =Number (prompt("ingrese calificacion de Prectica"));
+
+while (nombreAlumno !== "") {
+  n1 = Number(prompt("ingrese calificacion de Prectica"));
   console.log("Calificacion Practica :" + n1);
-  n2 =Number (prompt("ingrese calificacion de Problemas"));
+  n2 = Number(prompt("ingrese calificacion de Problemas"));
   console.log("Calificacion Problema :" + n2);
-  n3 =Number (prompt("ingrese calificacion de Teorica"));
+  n3 = Number(prompt("ingrese calificacion de Teorica"));
   console.log("Calificacion Teorica :" + n3);
-  if (n1> -1 && n2 > -1 && n3 > -1 && n1<==10 && n2<==10 && n3<==10) {
-    prom= n1 *0.10 + n2*0.50+ n3* 0.40;
-    console.log ("Nota final es :" + prom);
-    else {
-      console.log (" error , ud. ingreso un valor invalido");
-    }
+
+  if (n1 > -1 && n2 > -1 && n3 > -1 && n1 <= 10 && n2 <= 10 && n3 <= 10) {
+    prom = n1 * 0.1 + n2 * 0.5 + n3 * 0.4;
+    console.log("Nota final es :" + prom);
+  } else {
+    console.log(" error , ud. ingreso un valor invalido");
   }
+  nombreAlumno = prompt("Ingrese nombre del alumno:");
 }
